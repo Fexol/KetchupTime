3458ad4210e89d2551696d0763f9db06f9db605f
Fexol <fexol25@gmail.com>
2019-09-30 23:55:45 +0200

    imported project

---
+++ precache-manifest.ce5057c430f1f6d2c6cfe4046ecc5cb7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0ea1e559d1f5e5d29f85be62aa1c5f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e068db4572a3421a7625",
+    "url": "/static/css/main.cb2f33ea.chunk.css"
+  },
+  {
+    "revision": "b50c262cb0d918252f2c",
+    "url": "/static/js/2.36e1c0d5.chunk.js"
+  },
+  {
+    "revision": "e068db4572a3421a7625",
+    "url": "/static/js/main.16e1601c.chunk.js"
+  },
+  {
+    "revision": "f600d17f7e344995e243",
+    "url": "/static/js/runtime~main.8b1bcc0a.js"
+  },
+  {
+    "revision": "99c9d05dac819b5fd07a82d71d477927",
+    "url": "/static/media/Logo_geteilt0.99c9d05d.svg"
+  }
+]);
